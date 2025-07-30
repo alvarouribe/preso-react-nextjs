767b9de82d320f1e2ff43d79f0d1a5c808b72de9
Alvaro Uribe <iam@alvarouribe.cl>
2025-07-30 22:52:13 +1200

    Example 2

--- src/app/e2/page.tsx
+++ src/app/e2/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Example2() {
+    const [userData, setUserData] = useState({
+        name: 'Alvaro',
+        age: 0,
+        email: ''
+    });
+    const updateName = (event) => {
+        const newName = event.target.value;
+        console.log('Updating name to:', newName);
+    };
     return (
-        <h1>Conditional rendering</h1>
+        <main>
+            <h1>Example 2 - Objects updates</h1>
+           
+            <section>
+                <form>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        id="name" 
+                        placeholder="Insert your name" 
+                        onChange={updateName}
+                        value={userData.name} />
+                </form>
+            </section>
+        </main>
     )
 }
