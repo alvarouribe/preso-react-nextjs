a9c58b1127e8017dd24f0db09ed067e98e72c05d
Alvaro Uribe <iam@alvarouribe.cl>
2025-07-31 21:09:27 +1200

    e3: multiple states vs object

--- src/app/e3/page.tsx
+++ src/app/e3/page.tsx
@@ -3,27 +3,33 @@
 import { useState } from "react";
 
 export default function Example3() {
-    const [form, setForm] = useState({
-        firstName: '',
-        lastName: '',
-        email: ''
-    });
-        
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        setForm((prevForm) => ({
-            ...prevForm,
-            firstName: value // how to update via name?
-        }));
-    };
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+
     return (
         <main>
             <section>
-                <h1>Example 3 - Object changes </h1>
+                <h1>Example 3 - Object state instead of multiple small ones </h1>
                 <form className="flex flex-col gap-4">
-                    <input type="text" name="firstName" placeholder="Insert your firstName" onChange={handleChange} value={form.firstName} />
-                    <input type="text" name="lastName" placeholder="Insert your lastName" onChange={handleChange}  value={form.lastName}/>
-                    <input type="text" name="email" placeholder="Insert your email" onChange={handleChange} value={form.email} />
+                    <input 
+                        type="text" 
+                        name="firstName" 
+                        placeholder="Insert your firstName" 
+                        onChange={(e) => { setFirstName(e.target.value) }} 
+                        value={firstName} />
+                    <input 
+                        type="text" 
+                        name="lastName" 
+                        placeholder="Insert your lastName" 
+                        onChange={(e)=>{ setLastName(e.target.value) }}  
+                        value={lastName}/>
+                    <input 
+                        type="text" 
+                        name="email" 
+                        placeholder="Insert your email" 
+                        onChange={(e) => { setEmail(e.target.value) }} 
+                        value={email} />
                 </form>
             </section>
         </main>
