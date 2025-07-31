5cac87c89fb8c39617f2b448b2e82739fa7e9769
Alvaro Uribe <iam@alvarouribe.cl>
2025-07-31 16:15:57 +1200

    Example 4

--- src/app/e4/page.tsx
+++ src/app/e4/page.tsx
@@ -1,5 +1,25 @@
-export default function Ecample1() {
+"use client";
+import { useState } from "react";
+
+export default function Example4() {
+    const [price, setPrice] = useState(0);
+    console.log('Example 4 page mounted');
+
+    const handleClick = () => {
+        // setPrice(price + 1);
+        setPrice(0);
+    };
+
     return (
-        <div>E4</div>
+        <main>
+            <section>
+                <h1>Primitives vs Non primitives</h1>
+                <div className="mt-4 text-lg border p-2 rounded text-white size-30">
+                    Price {price}
+                </div>
+                <button className="cursor-pointer" type="button" onClick={handleClick}>Increse price</button>
+            </section>
+
+        </main>
     )
 }
