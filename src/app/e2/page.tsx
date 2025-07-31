7b42c5e9d28942d0b97c4425e83682cb22972e99
Alvaro Uribe <iam@alvarouribe.cl>
2025-07-31 20:57:51 +1200

    e2 changes

--- src/app/e2/page.tsx
+++ src/app/e2/page.tsx
@@ -5,18 +5,32 @@ import { useState } from "react";
 export default function Example2() {
     const [userData, setUserData] = useState({
         name: 'Alvaro',
-        age: 0,
+        age: 21,
         email: ''
     });
+    
     const updateName = (event) => {
         const newName = event.target.value;
         console.log('Updating name to:', newName);
+        // setUserData({
+        //     name: newName
+        // });
+        // console.log('userData:', userData);
     };
+
     return (
         <main>
            
             <section>
-            <h1>Example 2 - Objects updates</h1>
+                <h1>Example 2 - Objects updates</h1>
+                <p>
+                    Lets say we want to change the state when the input changes... 
+                    How can we fix this code:
+                </p>
+                <ul>
+                    <li>event type issue</li>
+                    <li>update the object data state</li>
+                </ul>
                 <form>
                     <input 
                         type="text" 
